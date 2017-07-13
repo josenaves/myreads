c0a12571420f296efbb71e5f3e02f06634c2476e
José Naves Moura Neto <josenaves@gmail.com>
2017-07-13 20:57:15 -0300

    feat: Add a Book functional component
    Move hardcoded books into state and make use of Book component to render them.

---
+++ src/Book.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react'
+
+function Book(props) {
+  const { title, author, image } = props
+  return (
+    <div className="book">
+      <div className="book-top">
+        <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `${image}` }}></div>
+        <div className="book-shelf-changer">
+          <select>
+            <option value="none" disabled>Move to...</option>
+            <option value="currentlyReading">Currently Reading</option>
+            <option value="wantToRead">Want to Read</option>
+            <option value="read">Read</option>
+            <option value="none">None</option>
+          </select>
+        </div>
+      </div>
+      <div className="book-title">{ title }</div>
+      <div className="book-authors">{ author }</div>
+    </div>
+  )
+}
+
+export default Book
