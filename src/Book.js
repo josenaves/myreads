6562f9b528b0aeddecb2882e770910d678a5c40c
José Naves Moura Neto <josenaves@gmail.com>
2017-07-17 22:39:20 -0300

    feat: Get books from external source
    Now the books are get from BooksAPI and put into App state

--- src/Book.js
+++ src/Book.js
@@ -1,6 +1,6 @@
 import React from 'react'
 
-function Book({ title, author, image }) {
+function Book({ title, author, image, shelf }) {
   return (
     <div className="book">
       <div className="book-top">
